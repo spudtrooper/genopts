c737a8e79445dfe22eac39359a1cdcadc6cdf44b
Jeff Palm <jeff@jeffpalm.com>
2022-01-04 07:29:50 -0500

    update docs

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ var (
 	prefixOptsType = flag.Bool("prefix_opts_type", false, "Prefix each option function with the --opts_type; --prefix takes precendence over --prefix_opts_type")
 	prefix         = flag.String("prefix", "", "Prefix each option with this string; --prefix takes precendence over --prefix_opts_type")
 	outfile        = flag.String("outfile", "", "Output result to this file in addition to printing to STDOUT")
-	update         = flag.Bool("update", false, "update all files recurisvely in the current directory")
+	update         = flag.Bool("update", false, "update all files recurisvely in the current directory or directory specified by --update_dir")
 	updateDir      = flag.String("update_dir", ".", "directory for update")
 )
 
@@ -32,7 +32,6 @@ func realMain() error {
 		if err != nil {
 			return err
 		}
-
 		if err := genopts.UpdateDir(dir, bin); err != nil {
 			return err
 		}
