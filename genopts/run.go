9d359676bc7837e1ab8e15782c6a52bf64277a15
Jeff Palm <jeff@jeffpalm.com>
2022-01-08 07:49:11 -0500

    remove log in update

--- genopts/run.go
+++ genopts/run.go
@@ -1,14 +1,11 @@
 package genopts
 
 import (
-	"log"
 	"os"
 	"os/exec"
-	"strings"
 )
 
 func run(dir, command string, args ...string) error {
-	log.Printf("running from %s: %s %s", dir, command, strings.Join(args, " "))
 	cmd := exec.Command(command, args...)
 	cmd.Dir = dir
 	cmd.Stdout = os.Stdout
